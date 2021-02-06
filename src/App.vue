90291489dd0096988e091445dbff8f80a740b6a6
Kentaro <huytran@Huys-iMac.local>
2021-02-06 23:01:53 +1100

    added reddit api requests

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,95 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <h2>Hello test</h2>
+
+  <div class="vidContainer">
+
+    <iframe id="youtubeContainer" v-bind:src="currentVideoUrl"></iframe>
+
+  </div>
+  <button v-on:click="prevVideo">Previous</button>
+
+  <button v-on:click="nextVideo">Next</button>
+
+
 </template>
 
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      videos: [],
+      currentVideoUrl: "",
+      currentVideoIndex:0
+    };
+  },
+  mounted() {
+    // get data
+    var xhr = new XMLHttpRequest();
+    var self = this;
+    // Setup our listener to process completed requests
+    xhr.onload = function () {
+      // Process our return data
+      if (xhr.status >= 200 && xhr.status < 300) {
+        if (xhr) {
+          let response = JSON.parse(xhr.response);
+
+          response.data.children.forEach((element) => {
+            if (
+              element.data.media &&
+              element.data.media.type == "youtube.com"
+            ) {
+              console.log(element);
+
+              const container = document
+                .createRange()
+                .createContextualFragment(element.data.media.oembed.html);
+
+              const iframe = document
+                .createRange()
+                .createContextualFragment(container.firstChild.data);
+
+              self.videos.push({
+                name: "xxx",
+                url: iframe.firstElementChild.getAttribute("src")
+              });
+            }
+          });
+
+          self.currentVideoUrl = self.videos[0].url;
+          console.log("found videos:" + self.videos.length)
+        }
+      } else {
+        // What do when the request fails
+        console.log("The request failed!");
+      }
+    };
+
+    xhr.open("GET", "https://www.reddit.com/r/videos.json");
+    xhr.send();
+  },
+  methods: {
+    previousVideo(){
+      this.currentVideoIndex--
+      this.currentVideoUrl =  this.videos[this.currentVideoIndex].url;
+
+    },
+    nextVideo() {
+      this.currentVideoIndex++;
+      this.currentVideoUrl =  this.videos[this.currentVideoIndex].url;
+    },
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
+
+#youtubeContainer{
+  width: 1000px;
+  height: 600px;
+
+}
+
 </style>
